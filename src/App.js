9dd714cd9f9ba711f29d19dacaf869d3bf45a5fb
Merit Student <student@meac.local>
2020-09-22 13:54:58 -0600

    Added Files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Nav from './components/Nav';
+import Yes from './components/Yes';
+import './App.css';
+
+function App() {
+  return (
+ <>
+  <Nav />
+  <Yes><i>Kass</i></Yes>
+  <h1>Hello</h1>
+  <h2>Hello 2</h2>
+ </>
+  );
+}
+
+export default App;
